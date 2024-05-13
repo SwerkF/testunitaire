9b38e8e5de0b150c9aaee6d3342d4b58fd215bf6
Raijin95120 <carlbrgs77@gmail.com>
2024-05-13 17:48:33 +0200

    add Cardcomponents and page eventlist

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CardComponent from './components/CardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CardComponent/>
+    </div>
+  );
+}
+
+export default App;
