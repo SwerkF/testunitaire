4633e9ca3e5800a9ea9cfe9fd6475f1e4b1b8310
Oliwer <swerk.pro@gmail.com>
2024-05-14 02:03:04 +0200

    Composants pdf + qrcode

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Reservations from './pages/Reservations';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/reservations" element={<Reservations></Reservations>} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
