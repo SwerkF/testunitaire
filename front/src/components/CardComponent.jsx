1f8fccfd56eb65fd9666207a2b6c32de70c3b64f
Raijin95120 <carlbrgs77@gmail.com>
2024-05-14 14:06:52 +0200

    chore: Update CardComponent styles and dependencies

--- front/src/components/CardComponent.jsx
+++ front/src/components/CardComponent.jsx
@@ -1,28 +1,41 @@
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
+import Modal from './ModalComponent.jsx'; // Import the Modal component
 import "../styles/CardComponent.scss";
 
 
-const CardComponent = ({ title, description, buttonText, imageUrl }) => {
+const CardComponent = ({ title, description, buttonText, imageUrl, id}) => {
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
     <div className="card-home">
       <img src={imageUrl} alt="" className="img-card-home" />
       <div className="card-content-home">
         <h2 className="h2-home">{title}</h2>
         <p className="p-home">{description}</p>
-        <a href="#" className="button-home a-home">
+        <button className="button-home a-home" onClick={() => setModalOpen(true)}>
           {buttonText}
           <span className="material-symbols-outlined">&#10132;</span>
-        </a>
+        </button>
       </div>
+      {isModalOpen && <Modal
+        title= {title}
+        onClose={() => setModalOpen(false)}
+        imageUrl={imageUrl}
+        id={id}
+      />}
     </div>
   );
 };
+
 
 CardComponent.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 
--- front/src/pages/EventList.jsx
+++ front/src/pages/EventList.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 */
 import CardComponent from "../components/CardComponent";
+import "../styles/EventList.css";
+
 const EventList = () => {
   /*const [events, setEvents] = useState([]);
     const { id } = useParams();
@@ -16,9 +18,14 @@ const EventList = () => {
     */
     return (
         <div>
-        <h1>Event List</h1>
-        <ul>
-            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" />
+        <h1 class="title-h1">Event List</h1>
+        <ul class= "event-container">
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="1"/>
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="3"/>
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="4"/>
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="5"/>
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="6" />
+            <CardComponent title="title" description="description brève de l'évènement" buttonText="plus d'info" imageUrl="https://img.freepik.com/photos-gratuite/lumiere-scene-rougeoyante-illumine-fans-rock-liesse-generes-par-ia_188544-37983.jpg" id="7"/>
         </ul>
         </div>
     );
