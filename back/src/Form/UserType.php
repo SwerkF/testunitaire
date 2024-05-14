bb0dcd60613cd84cd5dd57b1fe78d9a865b3e24d
Sarah Faye <118432192+fayesarah555@users.noreply.github.com>
2024-05-14 12:37:33 +0200

    Commit message

--- back/src/Form/UserType.php
+++ back/src/Form/UserType.php
@@ -6,21 +6,32 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 class UserType extends AbstractType
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
             ->add('roles')
-            ->add('password')
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false, // This field is not mapped to the entity
+            ])
             ->add('firstname')
             ->add('name')
             ->add('birthday', null, [
                 'widget' => 'single_text',
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -28,5 +39,12 @@ class UserType extends AbstractType
         $resolver->setDefaults([
             'data_class' => User::class,
         ]);
+    }
+
+    public function submitForm(User $user, string $plainPassword): void
+    {
+        // Encode and set the password
+        $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
     }
 }
