f13844b834d8ec8a280d9695ea2003472ca0c4db
Raijin95120 <carlbrgs77@gmail.com>
2024-05-16 15:56:36 +0200

    chore: Update API endpoint for fetching events in EventList.jsx and Reservation.jsx

--- front/src/components/CardComponent.jsx
+++ front/src/components/CardComponent.jsx
@@ -1,33 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 import Modal from './ModalComponent.jsx';
 
 import "../styles/CardComponent.scss";
+import axios from 'axios';
 
 
-const CardComponent = ({ title, description, buttonText, imageUrl, id}) => {
-
+const CardComponent = ({ event_date_id, event_id, ticket, date}) => {
+  const [event, setEvent] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000' + event_id)
+      .then((response) => {
+        setEvent(response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error fetching the events!', error);
+      });
+  }, []);
+  
+
 
   return (
     <div className="card-home">
       <div> 
-        <img src={imageUrl} alt="" className="img-card-home" />
+        <img src={event.imageUrl} alt="" className="img-card-home" />
       </div>
       <div className="card-content-home">
-        <h2 className="h2-home">{title}</h2>
-        <p className="p-home">{typeof description === 'string' && description.length > 40 ? description.slice(0, 40) + '...' : description}</p>
+        <h2 className="h2-home">{event.title}</h2>
+        <p className="p-home">{typeof event.description === 'string' && event.description.length > 40 ? event.description.slice(0, 40) + '...' : event.description}</p>
         <button className="button-home a-home" onClick={() => setModalOpen(true)}>
-          {buttonText}
+          {event.buttonText}
           <span className="material-symbols-outlined">&#10132;</span>
         </button>
       </div>
       {isModalOpen && <Modal
-        title= {title}
-        description={description}
+        title= {event.title}
+        description={event.description}
         onClose={() => setModalOpen(false)}
-        imageUrl={imageUrl}
-        id={id}
+        imageUrl={event.imageUrl}
+        id={event}
+        eventAge={event.minimum_age}
+        date= {date}
       />
       }
     </div>
@@ -36,11 +51,10 @@ const CardComponent = ({ title, description, buttonText, imageUrl, id}) => {
 
 
 CardComponent.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  buttonText: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired
+  event_date_id: PropTypes.string.isRequired,
+  event_id: PropTypes.string.isRequired,
+  ticket: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired
 };
 
 
